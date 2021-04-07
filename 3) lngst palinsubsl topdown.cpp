f6cbfaa4db77a2d4b9312b8f3f39489a7fd7cba7
JAI MEHTA <54937640+jaimehta1912@users.noreply.github.com>
2021-04-07 13:33:20 +0530

    Add files via upload

---
+++ 3) lngst palinsubsl topdown.cpp
@@ -0,0 +1,24 @@
+
+int longestPalinSubseq (string S)
+{
+    #include<string>
+    int x=S.length();int y=S.length();
+string s1=S;
+string t1=S;
+reverse(t1.begin(),t1.end());
+ int t[x+1][y+1];
+        memset (t,-1,sizeof(t));
+        for (int i=0;i<x+1;i++) t[i][0]=0;
+          for (int j=0;j<y+1;j++) t[0][j]=0;
+       
+       for (int i=1;i<x+1;i++)for (int j=1;j<y+1;j++){
+  // if(t[i][j!=-1)        return t[x][y];
+    // else{
+        if (s1[i-1]==t1[j-1]) t[i][j]= 1+t[i-1][j-1];
+        
+        else t[i][j]= max(t[i][j-1],t[i-1][j]);
+}
+       
+    //}
+             return t[x][y];
+}
